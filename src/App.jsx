bda944ec75359b03560395ed0407650b3cb91d53
Namkhang Tsamchoe <ntnamkhang@gmail.com>
2023-05-26 09:24:09 -0400

    second last step

--- src/App.jsx
+++ src/App.jsx
@@ -34,33 +34,35 @@ function App() {
   let eachPerson = persons.map(getEachElement);
   return (
     <>
-      <div className="nav">
-        <h3>Home</h3>
-        <h3>History</h3>
-        <h3>Board Members</h3>
-        <h3>Contact Us</h3>
-        <input type="text" placeholder="Search" />
+      <div className="box_container">
+        <div className="nav">
+          <h3>Home</h3>
+          <h3>History</h3>
+          <h3>Board Members</h3>
+          <h3>Contact Us</h3>
+          <input type="text" placeholder="Search" />
+        </div>
+        <br />
+        <h1>Meet the honor students</h1> <br />
+        <p className="long">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+          saepe nobis esse iusto soluta adipisci, quaerat qui in, magni
+          dignissimos amet quibusdam expedita dolore ipsa debitis est omnis nisi
+          praesentium. Porro, cum placeat? Odio incidunt quas minus animi
+          possimus facere voluptatem aliquid dolor, accusamus quaerat, molestias
+          soluta veritatis iusto. Iusto, maxime mollitia modi eligendi ratione
+          possimus voluptates dolores cum fuga. Laborum at impedit placeat autem
+          voluptatibus. Labore cupiditate et numquam sapiente dolore alias
+          doloribus est tempore, expedita doloremque voluptas, vitae pariatur
+          molestiae aliquid itaque eos officia maxime voluptatem qui? Veniam
+          magni nemo ut perspiciatis architecto fugit ad adipisci optio ab.
+        </p>
+        <div className="main">{eachPerson}</div>
+        <p className="short">
+          Check back for our next month's student of the year!
+        </p>
+        <div className="footer">Namkhang Tsamchoe@2023</div>
       </div>
-      <br />
-      <h1>Meet the honor students from the class of 2023</h1> <br />
-      <p className="long">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti saepe
-        nobis esse iusto soluta adipisci, quaerat qui in, magni dignissimos amet
-        quibusdam expedita dolore ipsa debitis est omnis nisi praesentium.
-        Porro, cum placeat? Odio incidunt quas minus animi possimus facere
-        voluptatem aliquid dolor, accusamus quaerat, molestias soluta veritatis
-        iusto. Iusto, maxime mollitia modi eligendi ratione possimus voluptates
-        dolores cum fuga. Laborum at impedit placeat autem voluptatibus. Labore
-        cupiditate et numquam sapiente dolore alias doloribus est tempore,
-        expedita doloremque voluptas, vitae pariatur molestiae aliquid itaque
-        eos officia maxime voluptatem qui? Veniam magni nemo ut perspiciatis
-        architecto fugit ad adipisci optio ab.
-      </p>
-      <div className="main">{eachPerson}</div>
-      <p className="short">
-        Check back for our next month's student of the year!
-      </p>
-      <div className="footer">Namkhang Tsamchoe@2023</div>
     </>
   );
 }
